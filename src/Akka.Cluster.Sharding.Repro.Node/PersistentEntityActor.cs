2c64b2433e82766cdba96eadc3e00566db789cd5
Aaron Stannard <aaron@petabridge.com>
2019-02-27 19:11:25 -0600

    added Petabridge.Cmd

---
+++ src/Akka.Cluster.Sharding.Repro.Node/PersistentEntityActor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Akka.Persistence;
+
+namespace Akka.Cluster.Sharding.Repro.Node
+{
+    public sealed class PersistentEntityActor : ReceivePersistentActor
+    {
+        private List<string> _currentItems = new List<string>();
+
+        public PersistentEntityActor(string persistenceId)
+        {
+            PersistenceId = persistenceId;
+
+            Recover<SnapshotOffer>(offer =>
+            {
+                if (offer.Snapshot is IEnumerable<string> strs)
+                {
+                    _currentItems = strs.ToList();
+                    Log.Info("Recovered [{0}]", string.Join(",", _currentItems));
+                }
+            });
+
+            Recover<string>(str =>
+            {
+                _currentItems.Add(str);
+
+                Log.Info("Recovered [{0}]", str);
+            });
+
+            Command<string>(str =>
+            {
+                if (LastSequenceNr % 10 == 0)
+                {
+                    SaveSnapshot(_currentItems.ToArray());
+                }
+
+                Persist(str, s =>
+                {
+                    _currentItems.Add(s);
+                    Log.Info("Added [{0}] - now have [{1}]", s, string.Join(",", _currentItems));
+                });
+            });
+
+            Command<SaveSnapshotSuccess>(success =>
+            {
+                DeleteMessages(success.Metadata.SequenceNr);
+                Log.Info("Successfully saved snapshot. Deleting messages up until [{0}]", success.Metadata.SequenceNr);
+            });
+        }
+
+        public override string PersistenceId { get; }
+    }
+}
--- src/Akka.Cluster.Sharding.Repro.Node/Program.cs
+++ src/Akka.Cluster.Sharding.Repro.Node/Program.cs
@@ -3,6 +3,9 @@ using System.IO;
 using Akka.Actor;
 using Akka.Bootstrap.Docker;
 using Akka.Configuration;
+using Akka.Util;
+using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Host;
 
 namespace Akka.Cluster.Sharding.Repro.Node
 {
@@ -25,10 +28,30 @@ namespace Akka.Cluster.Sharding.Repro.Node
                 "akka.persistence.snapshot-store.sql-server.connection-string = \"" + sqlConnectionString + "\"")
                 .WithFallback(config);
 
-            var actorSystem = ActorSystem.Create("ShardFight", sqlConnectionConfig.BootstrapFromDocker());
+            var actorSystem = ActorSystem.Create("ShardFight", sqlConnectionConfig.BootstrapFromDocker().WithFallback(ClusterSharding.DefaultConfig()));
 
+            ICancelable shardTask = null;
+
+            Cluster.Get(actorSystem).RegisterOnMemberUp(() =>
+            {
+                var sharding = ClusterSharding.Get(actorSystem);
+                var myShardRegion = sharding.Start("fubers", str => Props.Create(() => new PersistentEntityActor(str)),
+                    ClusterShardingSettings.Create(actorSystem).WithRole("shards"), new FuberMessageExtractor());
+
+                shardTask = actorSystem.Scheduler.Advanced.ScheduleRepeatedlyCancelable(TimeSpan.FromMilliseconds(250),
+                    TimeSpan.FromMilliseconds(250),
+                    () =>
+                    {
+                        myShardRegion.Tell(new FuberEnvelope(ThreadLocalRandom.Current.Next().ToString(), ThreadLocalRandom.Current.Next().ToString()));
+                    });
+            });
+
+            var pbm = PetabridgeCmd.Get(actorSystem);
+            pbm.RegisterCommandPalette(ClusterCommands.Instance); // enable cluster management commands
+            pbm.Start();
 
             actorSystem.WhenTerminated.Wait();
+            shardTask?.Cancel(); // should already be cancelled
             return 0;
         }
     }
