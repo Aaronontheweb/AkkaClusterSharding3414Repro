82fcfb357257d251688cebb6e550e5bf065d634f
Aaron Stannard <aaron@petabridge.com>
2019-03-18 17:37:27 -0500

    added SQL dump scripts for debugging purposes

---
+++ src/AkkaSql/dump-coordinator-journal.sql
@@ -0,0 +1,3 @@
+SELECT * FROM [dbo].[EventJournal] WHERE PersistenceId = "/system/sharding/fubersCoordinator/singleton/coordinator"
+ORDER BY SequenceNr AC
+GO
---
+++ src/AkkaSql/dump-coordinator-snapshots.sql
@@ -0,0 +1,3 @@
+SELECT * FROM [dbo].[SnapshotStore] WHERE PersistenceId = "/system/sharding/fubersCoordinator/singleton/coordinator"
+ORDER BY SequenceNr AC
+GO
